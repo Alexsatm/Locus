407749d028586189a19eac3778413bd76d82fd6b
alexsatm <dzamilovaalbina2@gmail.com>
2024-01-25 23:24:11 +0300

    feat: new burger

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-document.querySelector('.burger').addEventListener('click', function(){
-    this.classList.toggle('active');
-    document.querySelector('.menu').classList.toggle('open')
+document.getElementById('burger').addEventListener('click', function(){
+    document.querySelector('header').classList.toggle('open')
 })
