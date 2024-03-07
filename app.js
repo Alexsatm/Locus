5028d51b5a2acefc1847bae02dbb775742e228ba
alexsatm <dzamilovaalbina2@gmail.com>
2024-03-07 19:49:14 +0300

    add js form

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
-document.getElementById('burger').addEventListener('click', function(){
-    document.querySelector('header').classList.toggle('open')
-})
+document.getElementById('burger').addEventListener('click', function () {
+  document.querySelector('header').classList.toggle('open');
+});
 
 // contact form
+
+const nameError = document.querySelector('#name-error');
+const emailError = document.querySelector('#email-error');
+const submitError = document.querySelector('#submit-error');
+
+const form = document.getElementById('form-contact');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
+
+const validateName = () => {
+  const name = document.querySelector('#name').value;
+
+  if (name.lenght == 0) {
+    nameError.innerHTML = 'Требуется имя';
+    return false;
+  }
+
+  if (!name.match(/^([a-zа-яё\ ]+|\d+)$/gi)) {
+    nameError.innerHTML = 'Напишите полное имя';
+    return false;
+  }
+};
 
